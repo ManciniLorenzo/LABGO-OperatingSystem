df949952b693b0b781051a3c86029528773ede0e
MancioLollo <mancinilorenzo2000.lm@gmail.com>
2025-04-03 11:53:50 +0200

    Create packages.go

---
+++ packages.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt" // formattazione output
+	"math/rand" // generazione numeri casuali
+)
+
+func main(){
+	fmt.Println("Stampo un numero intero a caso: ", rand.Intn(100) )
+}
