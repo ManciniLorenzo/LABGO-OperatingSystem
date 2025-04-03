34d1c169d85f404dca2d796e129f035d391ecd5c
MancioLollo <mancinilorenzo2000.lm@gmail.com>
2025-04-03 12:08:26 +0200

    Create variabiliPRIMAinizializzate.go

---
+++ init/variabiliPRIMAinizializzate.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var a, b, c = true, false, true // dichiarazione con inizializzazione
+
+func main(){
+	var i int = 8 // dichiarazione con inizializzazione
+	fmt.Println(a, b, c, i)
+}
